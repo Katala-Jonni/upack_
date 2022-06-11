409dc3c7c0dffe898af71031dfc5fe3a093130e2
maksim <katala.jonni@yandex.ru>
2022-06-11 23:19:43 +0300

    Интеграция авторизации

--- fradmin/src/__fake-api__/auth-api.js
+++ fradmin/src/__fake-api__/auth-api.js
@@ -1,6 +1,7 @@
 import { createResourceId } from '../utils/create-resource-id';
 import { sign, decode, JWT_SECRET, JWT_EXPIRES_IN } from '../utils/jwt';
 import { wait } from '../utils/wait';
+import axios from 'axios';
 
 const users = [
   {
@@ -17,25 +18,86 @@ class AuthApi {
   async login({ email, password }) {
     await wait(500);
 
-    return new Promise((resolve, reject) => {
+    // const url = '/api/users/login';
+    // try {
+    //   const cUser = await axios.post(url, { email, password }, {
+    //     method: 'POST'
+    //   });
+    //   console.log('cUser', cUser);
+    // } catch (e) {
+    //   console.log('Error------>', e);
+    //   return 'Please check your email and password';
+    // }
+
+    // .catch(error => {
+    //   console.log(error);
+    // });
+
+
+    // .then(res => res)
+    //     .catch(error => {
+    //       console.log(error);
+    //     });
+
+    return new Promise(async (resolve, reject) => {
       try {
         // Find the user
         const user = users.find((_user) => _user.email === email);
 
-        if (!user || (user.password !== password)) {
-          reject(new Error('Please check your email and password'));
+        const url = '/api/users/login';
+        const cUser = await axios.post(url, { email, password }, {
+          method: 'POST'
+        })
+          .catch(error => {
+            return null;
+          });
+        // console.log('cUser', cUser);
+
+        // if (!user || (user.password !== password)) {
+        //   reject(new Error('Please check your email and password'));
+        //   return;
+        // }
+
+        // console.log('user--->', user);
+
+        if (!cUser || !cUser.data || !cUser.data.user) {
+          reject(new Error('Неправильный логин или пароль!'));
           return;
         }
 
         // Create the access token
-        const accessToken = sign({ userId: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
-
-        resolve(accessToken);
+        // const accessToken = sign({ userId: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+        const { token } = cUser.data.user;
+        // const accessToken = cUser.token;
+        // console.log('token', token);
+        resolve(token);
+        // resolve(accessToken);
       } catch (err) {
         console.error('[Auth Api]: ', err);
-        reject(new Error('Internal server error'));
+        reject(new Error('Что-то пошло не так! Попробуйте снова.'));
       }
     });
+
+    // return new Promise((resolve, reject) => {
+    //   try {
+    //     // Find the user
+    //     const user = users.find((_user) => _user.email === email);
+    //     const currentUser = users.find((_user) => _user.email === email);
+    //
+    //     if (!user || (user.password !== password)) {
+    //       reject(new Error('Please check your email and password'));
+    //       return;
+    //     }
+    //
+    //     // Create the access token
+    //     const accessToken = sign({ userId: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+    //
+    //     resolve(accessToken);
+    //   } catch (err) {
+    //     console.error('[Auth Api]: ', err);
+    //     reject(new Error('Internal server error'));
+    //   }
+    // });
   }
 
   async register({ email, name, password }) {
@@ -73,29 +135,47 @@ class AuthApi {
   }
 
   me(accessToken) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       try {
         // Decode access token
-        const { userId } = decode(accessToken);
+        // const { userId } = decode(accessToken);
 
         // Find the user
-        const user = users.find((_user) => _user.id === userId);
+        // const user = users.find((_user) => _user.id === userId);
+        const url = '/api/user';
+        const cUser = await axios.get(url, {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        })
+          .catch(error => {
+            return null;
+          });
 
-        if (!user) {
-          reject(new Error('Invalid authorization token'));
+        if (!cUser || !cUser.data || !cUser.data.user) {
+          reject(new Error('У Вас нет прав!'));
           return;
         }
 
+        // if (!user) {
+        //   reject(new Error('Invalid authorization token'));
+        //   return;
+        // }
+
+        const { user } = cUser.data;
+        delete user.token;
+
         resolve({
-          id: user.id,
-          avatar: user.avatar,
-          email: user.email,
-          name: user.name,
-          plan: user.plan
+          // id: user.id,
+          // avatar: user.avatar,
+          // email: user.email,
+          // name: user.name,
+          ...user,
+          plan: 'Premium'
         });
       } catch (err) {
-        console.error('[Auth Api]: ', err);
-        reject(new Error('Internal server error'));
+        console.error('[Auth Api]--->ME: ', err);
+        reject(new Error('Что-то пошло не так! Попробуйте снова.'));
       }
     });
   }
