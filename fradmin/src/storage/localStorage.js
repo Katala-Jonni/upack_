4f65b39f76ba4f76d27ae85fbb599c76473f3f0c
maksim <katala.jonni@yandex.ru>
2025-12-01 18:25:49 +0300

    edit localstorage

--- fradmin/src/storage/localStorage.js
+++ fradmin/src/storage/localStorage.js
@@ -1,12 +1,15 @@
 function loadStorage(sessionStorageKey) {
+    if (typeof window === 'undefined') return null;
     return window.localStorage.getItem(sessionStorageKey);
 }
 
 function saveStorage(sessionStorageKey, data = []) {
+    if (typeof window === 'undefined') return null;
     window.localStorage.setItem(sessionStorageKey, JSON.stringify(data));
 }
 
 function deleteStorage(sessionStorageKey) {
+    if (typeof window === 'undefined') return null;
     window.localStorage.removeItem(sessionStorageKey);
 }
 
