8058349f0eacf0fa936e090a326c5a48d6b1ca73
maksim <katala.jonni@yandex.ru>
2025-12-07 01:59:46 +0300

    добавил метадата

--- fradmin/src/app/(layout-1)/grocery-2/page.jsx
+++ fradmin/src/app/(layout-1)/grocery-2/page.jsx
@@ -1,13 +1,13 @@
 import { GroceryTwoPageView } from "pages-sections/grocery-2/page-view";
-export const metadata = {
-  title: "ЮПАК Петрозаводск",
-  description: `ЮПАК Петрозаводск Карелия - оптовые продажи для направлений медицина, бьюти сфера, общепит. Одноразовая посуда, хоз товары, пакеты, перчатки в Петрозаводске`,
-  authors: [{
-    name: "KJ",
-    url: "https://upack-10.ru/"
-  }],
-  keywords: ["хоз товары", "пакеты", "перчатки", "юпак Петрозаводск"]
-};
+// export const metadata = {
+//   title: "ЮПАК Петрозаводск",
+//   description: `ЮПАК Петрозаводск Карелия - оптовые продажи для направлений медицина, бьюти сфера, общепит. Одноразовая посуда, хоз товары, пакеты, перчатки в Петрозаводске`,
+//   authors: [{
+//     name: "KJ",
+//     url: "https://upack-10.ru/"
+//   }],
+//   keywords: ["хоз товары", "пакеты", "перчатки", "юпак Петрозаводск"]
+// };
 export default async function GroceryTwo() {
   return <GroceryTwoPageView />;
 }
