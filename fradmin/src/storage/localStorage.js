428ca1e64ebfa9633dcab1300dfb47453bf03145
maksim <katala.jonni@yandex.ru>
2025-12-06 22:32:56 +0300

    изменили главное лого и емаил данные

--- fradmin/src/storage/localStorage.js
+++ fradmin/src/storage/localStorage.js
@@ -1,16 +1,29 @@
 function loadStorage(sessionStorageKey) {
+  try {
     if (typeof window === 'undefined') return null;
     return window.localStorage.getItem(sessionStorageKey);
+  } catch (e) {
+    console.log('loadStorage', e);
+  }
+
 }
 
 function saveStorage(sessionStorageKey, data = []) {
+  try {
     if (typeof window === 'undefined') return null;
     window.localStorage.setItem(sessionStorageKey, JSON.stringify(data));
+  } catch (e) {
+    console.log('loadStorage', e);
+  }
 }
 
 function deleteStorage(sessionStorageKey) {
+  try {
     if (typeof window === 'undefined') return null;
     window.localStorage.removeItem(sessionStorageKey);
+  } catch (e) {
+    console.log('loadStorage', e);
+  }
 }
 
 export { loadStorage, saveStorage, deleteStorage };
